402fbe693dad5cdaaa91981d172d4b9a49ba2e49
.阿蛋 <a0802071398@gmail.com>
2025-05-15 12:45:41 +0800

    完成單一頁面的指定

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,19 @@ from .models import Todo
 # Create your views here.
 def todolist(requset):
     todos = Todo.objects.all()
+    # get唯一 filter篩選
+    # todos = Todo.objects.filter(id=11)
+
     print(todos)
     result = {"todos": todos}
     return render(requset, "todo/todolist.html", result)
+
+
+def viewtodo(requset, id):
+    todo = None
+    try:
+        todo = Todo.objects.get(id=id)
+    except Exception as e:
+        print(e)
+
+    return render(requset, "todo/viewtodo.html", {"todo": todo})
